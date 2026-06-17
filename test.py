7b974374c694df66a4842c418d35980b9807ec0f
Abdelfatah <abdelfatah3939@gmail.com>
2026-06-17 16:39:22 +0300

    Removed test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
-model = GPT2LMHeadModel.from_pretrained('gpt2')
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
-input_ids = tokenizer.encode("Scientists studying the human brain discovered that", return_tensors='pt')
-output = model.generate(input_ids, max_length=50, temperature=0.7, top_k=40, do_sample=True)
-print(tokenizer.decode(output[0]))
